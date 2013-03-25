03af1bd5e7d0db703e14d1ef09161a5a5e22fea2
John Smith <private@unknown.com>
2013-03-25 11:05:07 -0700

    Created blank module

---
+++ public_html/admin/language/english/module/mass_insert.php
@@ -0,0 +1,6 @@
+<?php
+$_['heading_title']    = 'Mass Insert Module';
+$_['text_module']      = 'Modules';
+$_['text_success']     = 'Success: You have modified module mass insert module!';
+$_['error_permission'] = 'Warning: You do not have permission to modify module mass insert module!';
+?>
