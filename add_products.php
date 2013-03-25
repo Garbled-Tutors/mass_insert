68af10675355258d55f24efe99a1809273da8ea9
John Smith <private@unknown.com>
2013-03-25 10:34:59 -0700

    Removed unnessisary fields from script

--- add_products.php
+++ add_products.php
@@ -4,14 +4,11 @@
 	mysql_connect(DB_HOSTNAME, DB_USERNAME, DB_PASSWORD) or die('Could not connect: ' . mysql_error());
 	mysql_select_db(DB_DATABASE) or die('Could not select database');
 	
-	$query = "INSERT INTO `oc_product` (`model`, `quantity`, `stock_status_id`, `image`, `manufacturer_id`, `shipping`, `price`, `points`, `tax_class_id`, `date_available`, `weight`, `weight_class_id`, `length`, `width`, `height`, `length_class_id`, `subtract`, `minimum`, `sort_order`, `status`, `date_added`, `date_modified`, `viewed`) VALUES ('New Model',0,8,'data/demo/samsung_tab_1.jpg',0,1,199.9900,0,9,'2011-04-25',0.00000000,1,0.00000000,0.00000000,0.00000000,1,1,1,1,1,'2011-04-26 08:57:34','2011-09-30 01:06:23',0);";
+	$query = "INSERT INTO `oc_product` (`model`, `quantity`, `stock_status_id`, `manufacturer_id`, `price`, `tax_class_id`, `date_available`) VALUES ('A',1,7,0,19.9,9,'2011-04-25');";
 	$result = mysql_query($query) or die('Query failed: ' . mysql_error());
 
-	$query = "INSERT INTO `oc_product_description` (`language_id`, `name`, `description`) VALUES (1,'Test Product','Great Product');";
+	$query = "INSERT INTO `oc_product_description` (`language_id`, `name`, `description`) VALUES (1,'Babel Fish','The Babel fish is small, yellow, leech-like, and probably the oddest thing in the universe. It feeds on brain wave energy, absorbing all unconscious frequencies and then excreting telepathically a matrix formed from the conscious frequencies and nerve signals picked up from the speech centres of the brain, the practical upshot of which is that if you stick one in your ear, you can instantly understand anything said to you in any form of language: the speech you hear decodes the brain wave matrix.');";
 	$result = mysql_query($query) or die('Query failed: ' . mysql_error());
-
-
-
 
 
 ?>
